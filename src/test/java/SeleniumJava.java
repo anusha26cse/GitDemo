513a81b67a7e0a3600c14131a06d8a43ed62468f
anusha <vadhipatianusha@gmail.com>
2021-02-18 17:48:40 +0530

    this is third person commit

--- src/test/java/SeleniumJava.java
+++ src/test/java/SeleniumJava.java
@@ -4,7 +4,10 @@ public class SeleniumJava {
     @Test
     public void BrowersAutomation()
     {
+
         System.out.println("Browser Automation");
+        System.out.println("web Automation");
+        System.out.println("App Automation");
     }
     @Test
     public void elementsUi()
