27e67d239394c5014633dcb05f2709a2345a405c
anusha <vadhipatianusha@gmail.com>
2021-02-18 17:10:46 +0530

    this is my first commit

---
+++ src/test/java/RestAPITest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+    @Test
+    public void posJira()
+    {
+        System.out.println("postJira");
+    }
+    @Test
+    public void deleteTwitter(){
+        System.out.println("delete twitter");
+    }
+}
---
+++ src/test/java/SeleniumJava.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class SeleniumJava {
+    @Test
+    public void BrowersAutomation()
+    {
+        System.out.println("Browser Automation");
+    }
+    @Test
+    public void elementsUi()
+    {
+System.out.println("Elements Ui");
+    }
+
+
+
+}
